9de5db7793670dbc7823130b1de0a6ac50a7598c
Bayulo <bayugalordley@gmail.com>
2025-08-05 13:15:16 +0100

    update

--- front-end/intern/assets/script.js
+++ front-end/intern/assets/script.js
@@ -1,8 +1,14 @@
 function showNavbar(){
-    const navBar = document.querySelector("#side_nav_bar");
-    navBar.style.display = "inline-block";
-    navBar.style.width = "29vw";
-    document.querySelector("#side_nav_bar li").style.width = "28vw";
-    const responsive_nav = document.querySelector("#responsive_nav");
-    responsive_nav.style.display = "none";
+    let count = 0;
+    count++;
+    while (count===1){
+        document.querySelector("#open/close_nav").setAttribute("src","/front-end/intern/assets/images/close_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png")
+        const bar_items = document.querySelectorAll(".view_on_mobile");
+        const responsive_nav = document.querySelector("#responsive_nav");
+        responsive_nav.style.width = "35vw";
+
+        
+        count = 0;
+    }
+    
 }
