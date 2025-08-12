43aa31e62acc9bb970ba082264c56a4b5c64e330
Bayulo <bayugalordley@gmail.com>
2025-08-12 11:45:00 +0100

    Update 12-08-2025 12:00pm morning push

--- front-end/intern/assets/script.js
+++ front-end/intern/assets/script.js
@@ -1,52 +1,85 @@
 // Global toggle variable
 let isOpen = false;
 
-function showNavbar() {
+function show_menu(){
     const icon = document.getElementById("open_close_nav");
     const items = document.querySelectorAll(".view_on_mobile");
     const responsive_nav = document.getElementById("responsive_nav");
 
+    icon.setAttribute("src", "/front-end/intern/assets/images/close_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png");
+    items.forEach(item => {item.style.display = "block"; item.style.height = "6vh"});
+    responsive_nav.style.width = "30vw";
+    responsive_nav.style.position = "absolute";
+}
+
+function hide_menu(){
+    const icon = document.getElementById("open_close_nav");
+    const items = document.querySelectorAll(".view_on_mobile");
+    const responsive_nav = document.getElementById("responsive_nav");
+
+    icon.setAttribute("src", "/front-end/intern/assets/images/menu_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png");
+    items.forEach(item => item.style.display = "none");
+    responsive_nav.style.width = "9vw";
+    responsive_nav.style.position = "static";
+}
+
+function showNavbar() {
     if (!isOpen) {
         // Show menu
-        icon.setAttribute("src", "/front-end/intern/assets/images/close_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png");
-        items.forEach(item => item.style.display = "block");
-        responsive_nav.style.width = "35vw";
-        responsive_nav.style.position = "absolute";
-
-    } else {
-        // Hide menu
-        icon.setAttribute("src", "/front-end/intern/assets/images/menu_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png");
-        items.forEach(item => item.style.display = "none");
-        responsive_nav.style.width = "9vw";
-        responsive_nav.style.position = "relative";
+        popupBackground();
+        show_menu();
+    } else { // Hide menu
+        hide_menu();
     }
-
     isOpen = !isOpen; // Flip the toggle
 }
 
 
-let auto_close = 0;
-
 function showProfile(){
+    popup_is_open = false;
     const profile_icon = document.getElementById("profile_info");
     profile_icon.style.display = "flex";
-    auto_close ++;
+    popupBackground();
 }
 
 function closeProfile(){
+    popup_is_open = true;
     const close_profile = document.getElementById("profile_info");
     close_profile.style.display = "none";
+    popupBackground();
 }
 
 function showSupervisor(){
     const supervisor = document.getElementById("supervisor_info");
     supervisor.style.display = "flex";
-    auto_close ++;
+    popup_is_open = false;
+    popupBackground();
+    hide_menu();
 }
 
 function closeSupervisor(){
+    popup_is_open = true;
     const supervisor = document.getElementById("supervisor_info");
     supervisor.style.display = "none";
+    popupBackground();
+
+}
+let popup_is_open = false;
+function popupBackground(){
+    const background = document.getElementById("back_blur");
+    if(!popup_is_open){
+        background.style.display = "block";
+        background.addEventListener("click", function closePopup(){
+            background.style.display = "none";
+            hide_menu();
+            popup_is_open = false;
+        })
+    }
+    else{
+        background.style.display = "none";
+        hide_menu();
+    }
+    
 }
 // function close_all_open(){
 //     auto_close !== 0 ? (closeSupervisor() && closeProfile() && auto_close = 0) : continue;
